754babc3277fb9e1a1f8359a59669ae46dc915de
Drake Costa <drake@saeris.io>
2023-07-25 15:04:29 -0700

    wip: Update imports for profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,38 +1,38 @@
-import { auth } from '@/auth'
-import ProfileForm from '@/components/profile-form'
-import { getPrompts } from '../actions'
-import { redirect } from 'next/navigation'
+import { redirect } from "next/navigation";
+import { auth } from "../../auth";
+import { ProfileForm } from "../../components/forms/ProfileForm";
+import { getPrompts } from "../../actions/getPrompts";
 
 type Prompts = {
-  prompt_name: string
-  prompt_body: string
-}[]
+  prompt_name: string;
+  prompt_body: string;
+}[];
 
 export default async function ProfilePage() {
-  const session = await auth()
-  const user = session?.user
+  const session = await auth();
+  const user = session?.user;
   if (!user) {
-    redirect('/sign-in')
+    redirect(`/sign-in`);
   }
 
-  const prompts = await getPrompts(user) as Prompts
+  const prompts = (await getPrompts(user)) as Prompts;
 
   return (
     <div className="flex-1 space-y-6">
-      <div className="px-4 py-6 border-b bg-background md:px-6 md:py-8">
-        <div className="max-w-2xl mx-auto md:px-6">
+      <div className="border-b bg-background px-4 py-6 md:px-6 md:py-8">
+        <div className="mx-auto max-w-2xl md:px-6">
           <div className="space-y-1 md:-mx-8">
             <h1 className="text-2xl font-bold">
-              {user?.user_metadata.name}&apos;s Profile
+              {user.user_metadata.name}&apos;s Profile
             </h1>
             <div className="text-sm text-muted-foreground">
-              {user?.user_metadata.email}
+              {user.user_metadata.email}
             </div>
           </div>
         </div>
       </div>
       <div className="px-4 py-6 md:px-6">
-        <div className="max-w-2xl mx-auto rounded-lg bg-background border p-6">
+        <div className="mx-auto max-w-2xl rounded-lg border bg-background p-6">
           <div className="space-y-6">
             <div className="space-y-6">
               <ProfileForm user={user} prompts={prompts} />
@@ -41,5 +41,5 @@ export default async function ProfilePage() {
         </div>
       </div>
     </div>
-  )
+  );
 }
