2676d04129898d3be36d9b9813d47916a6f35ce4
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-08-08 20:19:12 -0700

    refactor(process-search-results) change metaphorKey environment variable, remove console log; chore(functions) delete search-the-web file

--- lib/functions/process-search-results.ts
+++ lib/functions/process-search-results.ts
@@ -2,8 +2,8 @@ import Metaphor from 'metaphor-node'
 import { envs } from '@/constants/envs';
 import { DocumentContent, Result } from 'metaphor-node';
 
-const metaphorKey = envs.METAPHOR_API_KEY
-export const metaphor = new Metaphor(metaphorKey as string)
+const metaphorKey = process.env.NEXT_PUBLIC_METAPHOR_API_KEY as string
+export const metaphor = new Metaphor(metaphorKey)
 
 export async function processSearchResult(result: Result): Promise<DocumentContent> {
     // Use the Metaphor instance to get the contents of the first result
@@ -11,7 +11,6 @@ export async function processSearchResult(result: Result): Promise<DocumentConte
 
     // Get the contents of the first result
     const resultContent = contentResponse.contents[0];
-    console.log('contentResponse.contents', contentResponse.contents)
 
     return resultContent;
 };
