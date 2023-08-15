02a7035d91861d4acb426772d3aa6b4f2621201c
Alec Dewitz <atdewitz@gmail.com>
2023-08-14 22:25:39 -0700

    visualize components for different function calls

--- components/chat-message.tsx
+++ components/chat-message.tsx
@@ -1,24 +1,83 @@
 // Inspired by Chatbot-UI and modified to fit the needs of this project
 // @see https://github.com/mckaywrigley/chatbot-ui/blob/main/components/Chat/ChatMessage.tsx
 
-import { Message } from 'ai';
-import remarkGfm from 'remark-gfm';
-import remarkMath from 'remark-math';
+import { Message } from 'ai'
+import remarkGfm from 'remark-gfm'
+import remarkMath from 'remark-math'
 
-import { ChatMessageActions } from '@/components/chat-message-actions';
-import { MemoizedReactMarkdown } from '@/components/markdown';
-import { CodeBlock } from '@/components/ui/codeblock';
+import { ChatMessageActions } from '@/components/chat-message-actions'
+import { MemoizedReactMarkdown } from '@/components/markdown'
+import { CodeBlock } from '@/components/ui/codeblock'
 import {
   IconCheck,
   IconOpenAI,
   IconSpinner,
   IconUser
-} from '@/components/ui/icons';
-import { cn } from '@/lib/utils';
+} from '@/components/ui/icons'
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import { useState } from 'react'
+import { Button } from './ui/button'
+import { ArrowDownIcon } from '@radix-ui/react-icons'
 
 export interface ChatMessageProps {
   message: Message
   functionCallString?: string
+}
+
+const RenderFunctionMessage = ({ message }: ChatMessageProps) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  if (message.name === 'searchTheWeb') {
+    const parsedContent = JSON.parse(message.content)?.results
+
+    return (
+      <ul className="list-decimal space-y-4 list-inside">
+        {parsedContent?.results?.map((result: any) => (
+          <li key={result.id}>
+            <span className="font-medium">{result.title}</span>:{' '}
+            <Link target="_blank" className="underline" href={result.url}>
+              {result.url}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    )
+  } else if (message.name === 'processSearchResult') {
+    const result = JSON.parse(message.content)?.results
+    const link = JSON.parse(message.content)?.link
+
+    return (
+      <div className="">
+        <div
+          className={cn(
+            `transition-all duration-300 overflow-hidden`,
+            isOpen ? 'max-h-screen' : 'max-h-36'
+          )}
+        >
+          <Link href={result.url} target="_blank" className="font-medium">
+            {result?.title}
+          </Link>
+          :
+          <p className="whitespace-pre-line text-muted-foreground">
+            {result.extract
+              .replace(/(<([^>]+)>)/gi, ' ')
+              .trim()
+              .replace(/\n\s*\n\s*\n/g, '\n\n')}
+          </p>
+        </div>
+        {!isOpen && (
+          <div className="absolute -mb-8 h-24 justify-center shadow-lg from-[#18181a] bg-gradient-to-t to-transparent bg-opacity-0 items-end bottom-0 left-0 right-0 flex">
+            <Button onClick={() => setIsOpen(!isOpen)} className="mb-4">
+              Show more <ArrowDownIcon className="ml-1" />
+            </Button>
+          </div>
+        )}
+      </div>
+    )
+  }
+
+  return <></>
 }
 
 export function ChatMessage({ message, ...props }: ChatMessageProps) {
@@ -38,7 +97,7 @@ export function ChatMessage({ message, ...props }: ChatMessageProps) {
     if (message.function_call.name === 'searchTheWeb') {
       content = 'Searching the web...'
     } else if (message.function_call.name === 'processSearchResult') {
-      content = 'Reading a result...'
+      content = 'Reading top result...'
     }
   }
 
@@ -65,8 +124,11 @@ export function ChatMessage({ message, ...props }: ChatMessageProps) {
       <div className="ml-4 flex-1 space-y-2 overflow-hidden px-1">
         {message.role === 'function' && message.function_call ? (
           renderFunctionCall()
+        ) : message.role === 'function' ? (
+          <RenderFunctionMessage message={message} />
         ) : (
           <MemoizedReactMarkdown
+            linkTarget={'_blank'}
             className="prose break-words dark:prose-invert prose-p:leading-relaxed prose-pre:p-0"
             remarkPlugins={[remarkGfm, remarkMath]}
             components={{
